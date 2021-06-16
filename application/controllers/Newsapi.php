1cb5c6d231a4144b2ca468d4567afc002bef0e62
afagawin <84000509+afagawin@users.noreply.github.com>
2021-06-16 21:52:39 +0800

    Update Newsapi.php

--- application/controllers/Newsapi.php
+++ application/controllers/Newsapi.php
@@ -32,7 +32,7 @@
 	 public function index_put($id)
 	 {
 		 $input = $this->put();
-		 $this->db->update('news', $input, array('id'=>$id));
+		 $this->db->set('news', $input, array('id'=>$id));
 		 $this->response(['News updated successfully.'], 200);
 	 }
 	public function index_delete($id)
